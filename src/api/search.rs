3c2462048bfd6bab536a944bea4e0975ab8ebdda
Luis Ball <lball@imgix.com>
2021-11-03 22:32:34 -0700

    feat(search): add repo filter

--- src/api/search.rs
+++ src/api/search.rs
@@ -71,3 +71,43 @@ pub async fn fetch_pull_requests_matching(
 
     Ok(join_all(responses).await)
 }
+pub async fn fetch_matching_pull_requests_from_repository(
+    pattern: &str,
+    repository: &str,
+    credentials: &Credentials,
+) -> Result<Vec<PullRequest>, Box<dyn Error>> {
+
+    let client = reqwest::Client::new();
+
+    let request = api::base_request(
+        &client,
+        &credentials,
+        "https://api.github.com/search/issues",
+    )
+    .query(&[("q", format!("{} in:title repo:{}", pattern, repository))]);
+
+    let items = request.send().await?.json::<SearchResponse>().await?.items;
+
+    let item_futures = items.into_iter().map(|item| {
+        api::base_request(&client, &credentials, &item.url.replace("issues", "pulls")).send()
+    });
+
+    // The `unwrap`s are required here because both `reqwest::send` and `reqwest::json` return a `Result` which has
+    // to be unwrapped after the future has been `await`ed on.
+    let items = join_all(item_futures)
+        .await
+        .into_iter()
+        .map(|item| item.unwrap());
+
+    let responses: Vec<_> = join_all(items.map(|item| item.json::<PullRequest>()))
+        .await
+        .into_iter()
+        .map(|item| async {
+            let pr = item.unwrap();
+            let pr = pr.fetch_reviews(credentials).await.unwrap();
+            pr
+        })
+        .collect();
+
+    Ok(join_all(responses).await)
+}
